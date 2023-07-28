a06ce624937f08d012ecbef920b4703f89631caf
suryaprakashgithubj <113435133+suryaprakashgithubj@users.noreply.github.com>
2023-07-28 12:08:45 +0530

    Add files via upload

---
+++ MICROSERVICES/Feedback_Service/src/main/java/com/springapp/demo/Entity/Feedback.java
@@ -0,0 +1,75 @@
+package com.springapp.demo.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+
+@Entity
+public class Feedback {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "user_email")
+	private String email;
+	
+	public Feedback(Long id, String email, String name, String comment) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.name = name;
+		this.comment = comment;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	private String name;
+	
+	private String comment;
+	
+	public Feedback()
+	{
+		
+	}
+	
+}
